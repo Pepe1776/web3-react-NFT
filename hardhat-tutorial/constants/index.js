8331946a48740396de02dfd2b3795b169b911bfb
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-03-29 22:43:37 -0700

    initial

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x7d524DC5f7c1e7862B65cA83eA02Cfe37e95D339";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
